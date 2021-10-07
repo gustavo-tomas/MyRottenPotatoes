779bcaeadca262cdab9ef33b69dab77b5d839cc6
Gustavo Tomás <guga.tomas10@gmail.com>
2021-10-07 15:23:33 -0300

    finished section 8.6 & 8.7

--- myrottenpotatoes/app/models/movie.rb
+++ myrottenpotatoes/app/models/movie.rb
@@ -1,7 +1,13 @@
 class Movie < ActiveRecord::Base
+    class Movie::InvalidKeyError < StandardError ; end 
+    def self.find_in_tmdb(string)
+        begin
+            Tmdb::Movie.find(string)
+        rescue Tmdb::InvalidApiKeyError
+            raise Movie::InvalidKeyError, 'Invalid API key'
+        end
+    end
     def self.all_ratings
         ['G','PG','PG-13','R','NC-17']
-    end
-    def self.find_in_tmdb(movie)
     end
 end
---
+++ myrottenpotatoes/spec/models/movie_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Movie do
+  describe 'searching Tmdb by keyword' do
+    context 'with valid API key' do 
+      it 'calls Tmdb with title keywords' do
+        expect(Tmdb::Movie).to receive(:find).with('Inception')
+        Movie.find_in_tmdb('Inception')
+      end
+    end
+    context 'with invalid API key' do
+      before(:each) do
+        allow(Tmdb::Movie).to receive(:find).and_raise(Tmdb::InvalidApiKeyError)
+      end
+      it 'raises an InvalidKeyError' do
+        expect { Movie.find_in_tmdb('Inception') }.
+          to raise_error(Movie::InvalidKeyError)
+      end
+    end
+  end
+end
